118e44817d21ef8b4ae08b77bfc4e4cbc1bb0a60
Antony Holmes <antony@antonybholmes.com>
2024-10-18 23:24:02 -0400

    fix: Bug fixes and updates.

--- excel.go
+++ excel.go
@@ -2,8 +2,8 @@ package sys
 
 import (
 	"bytes"
-	"fmt"
 
+	"github.com/rs/zerolog/log"
 	"github.com/xuri/excelize/v2"
 )
 
@@ -17,50 +17,56 @@ func XlsxToText(input []byte, index int, header int) (*Table, error) {
 	r := bytes.NewReader(input)
 
 	f, err := excelize.OpenReader(r) // .OpenFile("Book1.xlsx")
+
 	if err != nil {
-		fmt.Println(err)
 		return nil, err
 	}
 
 	defer func() {
 		// Close the spreadsheet.
-		if err := f.Close(); err != nil {
-			fmt.Println(err)
+		err := f.Close()
+
+		if err != nil {
+			log.Debug().Msgf("err closing xlsx: %s", err)
 		}
 	}()
 
 	// Get all the rows in the Sheet1.
 	rows, err := f.GetRows("Sheet1")
 	if err != nil {
-		fmt.Println(err)
+
 		return nil, err
 	}
 
+	n := len(rows[0])
 	colStart := max(index+1, 0)
-	dataStart := max(header+1, 0)
-	cols := len(rows) - colStart - 1
+	//dataStart := max(header+1, 0)
+	cols := len(rows) - colStart
 
 	indexNames := make([][]string, 0, 100)
 	columns := make([][]string, cols)
 	data := make([][]string, 0, 100)
 
 	for i := 0; i < cols; i++ {
-		columns[i] = make([]string, header)
+		columns[i] = make([]string, header+1)
 	}
+
+	//log.Debug().Msgf("hmm:%d %d %d %d", cols, len(rows), colStart, header)
 
 	for ri, row := range rows {
 		if ri <= header {
-			for i := colStart; i < cols; i++ {
+			for i := colStart; i < n; i++ {
+				log.Debug().Msgf("%d %d", i, i-colStart)
 				columns[i-colStart][ri] = row[i]
 			}
 		} else {
-			indexNames = append(indexNames, make([]string, dataStart))
+			indexNames = append(indexNames, make([]string, colStart))
 
-			for i := 0; i < index; i++ {
+			for i := 0; i <= index; i++ {
 				indexNames[len(indexNames)-1][i] = row[i]
 			}
 
-			data = append(data, row[colStart:cols])
+			data = append(data, row[colStart:n])
 
 		}
 	}
