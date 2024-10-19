75902aa917043ab40b7cf3f2651ec4b1c0865351
Antony Holmes <antony@antonybholmes.com>
2024-10-18 23:28:23 -0400

    fix: Bug fixes and updates.

--- test_xlsx.sh
+++ test_xlsx.sh
@@ -1,9 +1,9 @@
-curl -H "Content-Type: application/json" -X POST --data "{\"xlsx\":\"$(base64 -w 0 test.xlsx)\",\"indexCol\":0,\"header\":0}" localhost:8080/util/convert/xlsx
+curl -H "Content-Type: application/json" -X POST --data "{\"b64xlsx\":\"$(base64 -w 0 test.xlsx)\",\"indexCol\":0,\"header\":0}" localhost:8080/util/convert/xlsx
 
-curl -H "Content-Type: application/json" -X POST --data "{\"xlsx\":\"$(base64 -w 0 test.xlsx)\",\"indexCol\":-1,\"header\":-1}" localhost:8080/util/convert/xlsx
+curl -H "Content-Type: application/json" -X POST --data "{\"b64xlsx\":\"$(base64 -w 0 test.xlsx)\",\"indexCol\":-1,\"header\":-1}" localhost:8080/util/convert/xlsx
 
-curl -H "Content-Type: application/json" -X POST --data "{\"xlsx\":\"$(base64 -w 0 test.xlsx)\",\"indexCol\":0,\"header\":-1}" localhost:8080/util/convert/xlsx
+curl -H "Content-Type: application/json" -X POST --data "{\"b64xlsx\":\"$(base64 -w 0 test.xlsx)\",\"indexCol\":0,\"header\":-1}" localhost:8080/util/convert/xlsx
 
-curl -H "Content-Type: application/json" -X POST --data "{\"xlsx\":\"$(base64 -w 0 test.xlsx)\",\"indexCol\":-1,\"header\":0}" localhost:8080/util/convert/xlsx
+curl -H "Content-Type: application/json" -X POST --data "{\"b64xlsx\":\"$(base64 -w 0 test.xlsx)\",\"indexCol\":-1,\"header\":0}" localhost:8080/util/convert/xlsx
 
-curl -H "Content-Type: application/json" -X POST --data "{\"xlsx\":\"$(base64 -w 0 test.xlsx)\",\"indexCol\":1,\"header\":1}" localhost:8080/util/convert/xlsx
+curl -H "Content-Type: application/json" -X POST --data "{\"b64xlsx\":\"$(base64 -w 0 test.xlsx)\",\"indexCol\":1,\"header\":1}" localhost:8080/util/convert/xlsx
