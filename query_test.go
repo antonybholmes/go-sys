7aeb8359e26551c2efed0bb6a0bf4f23aff6ab23
Antony Holmes <antony@antonyholmes.dev>
2025-06-16 10:58:34 -0400

    Fixed: Bug fixes and updates.

--- query_test.go
+++ query_test.go
@@ -6,7 +6,7 @@ import (
 )
 
 func TestAdd(t *testing.T) {
-	sql, args := SqlBoolQuery("A+B,=C+(D+E)", func(placeholder string, exact bool) string {
+	resp, _ := SqlBoolQuery("A+B,=C+(D+E)", func(placeholder uint, matchType MatchType) string {
 
 		// if exact {
 		// 	return "(gex.gene_symbol = ? OR gex.ensembl_id = ?)"
@@ -14,15 +14,15 @@ func TestAdd(t *testing.T) {
 		// 	return fmt.Sprintf("(gex.gene_symbol LIKE %s OR gex.ensembl_id LIKE %s)", placeholder, placeholder)
 		// }
 
-		return placeholder //fmt.Sprintf("(gex.gene_symbol LIKE %s OR gex.ensembl_id LIKE %s)", placeholder, placeholder)
+		return fmt.Sprintf("?%d", placeholder) //fmt.Sprintf("(gex.gene_symbol LIKE %s OR gex.ensembl_id LIKE %s)", placeholder, placeholder)
 
 	})
 
 	//fmt.Println("A+B,=C+(D+E)")
-	fmt.Println(sql)
-	fmt.Println(args)
+	fmt.Println(resp.Sql)
+	fmt.Println(resp.Args...)
 
-	if sql != "((?1 AND ?2) OR (=?3 AND (?4 AND ?5)))" {
-		t.Errorf("SqlBoolQuery = %s; want %s", sql, "((?1 AND ?2) OR (?3 AND (?4 AND ?5)))")
+	if resp.Sql != "((?1 AND ?2) OR (=?3 AND (?4 AND ?5)))" {
+		t.Errorf("SqlBoolQuery = %s; want %s", resp.Sql, "((?1 AND ?2) OR (?3 AND (?4 AND ?5)))")
 	}
 }
