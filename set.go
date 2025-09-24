be24982f228ac18c64ae01fb0101498e48f66aae
Antony Holmes <antony@antonyholmes.dev>
2025-09-24 11:34:54 -0400

    Changed: Improved Set and switched JWT roles to array

--- set.go
+++ set.go
@@ -3,39 +3,49 @@ package sys
 import "slices"
 
 // Implementation of a set
-type Set[T comparable] map[T]struct{}
+type Set[T comparable] struct {
+	items map[T]struct{}
+}
 
 func NewSet[T comparable]() *Set[T] {
-	s := make(Set[T])
-	return &s
+	return &Set[T]{items: make(map[T]struct{})}
 }
 
 // Adds length function
 func (s *Set[T]) Len() int {
-	return len(*s)
+	return len(s.items)
 }
 
 // Adds to the set
 func (s *Set[T]) Add(v T) {
-	(*s)[v] = struct{}{}
+	s.items[v] = struct{}{}
 }
 
 // Removes an item from the set
 func (s *Set[T]) Remove(v T) {
-	delete(*s, v)
+	delete(s.items, v)
 }
 
 // Returns a boolean value describing if the value exists in the set
 func (s *Set[T]) Has(v T) bool {
-	_, ok := (*s)[v]
+	_, ok := s.items[v]
 	return ok
+}
+
+func (s *Set[T]) Keys() []T {
+	keys := make([]T, 0, len(s.items))
+	for key := range s.items {
+		keys = append(keys, key)
+	}
+
+	return keys
 }
 
 // Returns the insection of a map with another
 func (s *Set[T]) Intersect(s2 *Set[T]) *Set[T] {
 	ret := NewSet[T]()
 
-	for k := range *s {
+	for k := range s.items {
 		if s2.Has(k) {
 			ret.Add(k)
 		}
@@ -47,11 +57,11 @@ func (s *Set[T]) Intersect(s2 *Set[T]) *Set[T] {
 func (s *Set[T]) Union(s2 *Set[T]) *Set[T] {
 	ret := NewSet[T]()
 
-	for k := range *s {
+	for k := range s.items {
 		ret.Add(k)
 	}
 
-	for k := range *s2 {
+	for k := range s2.items {
 		ret.Add(k)
 	}
 
@@ -59,14 +69,14 @@ func (s *Set[T]) Union(s2 *Set[T]) *Set[T] {
 }
 
 func (s *Set[T]) Update(values *Set[T]) *Set[T] {
-	for v := range *values {
+	for v := range values.items {
 		s.Add(v)
 	}
 
 	return s
 }
 
-func (s *Set[T]) UpdateList(values []T) *Set[T] {
+func (s *Set[T]) UpdateFromList(values []T) *Set[T] {
 	for _, v := range values {
 		s.Add(v)
 	}
@@ -74,15 +84,64 @@ func (s *Set[T]) UpdateList(values []T) *Set[T] {
 	return s
 }
 
-func StringSetSort(s *Set[string]) []string {
+type StringSet struct {
+	*Set[string]
+}
 
-	sortedGenes := make([]string, 0, len(*s))
+func NewStringSet() *StringSet {
+	return &StringSet{
+		Set: NewSet[string](), // assuming NewSet initializes the map
+	}
+}
 
-	for key := range *s {
-		sortedGenes = append(sortedGenes, key)
+// Specialized keys that returns sorted keys
+func (s *StringSet) Keys() []string {
+	keys := make([]string, 0, len(s.items))
+	for key := range s.items {
+		keys = append(keys, key)
 	}
 
-	slices.Sort(sortedGenes)
+	slices.Sort(keys)
 
-	return sortedGenes
+	return keys
 }
+
+func (s *StringSet) Intersect(s2 *StringSet) *StringSet {
+	ret := NewStringSet()
+
+	for k := range s.items {
+		if s2.Has(k) {
+			ret.Add(k)
+		}
+	}
+
+	return ret
+}
+
+func (s *StringSet) Update(values *StringSet) *StringSet {
+	for v := range values.items {
+		s.Add(v)
+	}
+
+	return s
+}
+
+func (s *StringSet) UpdateFromList(values []string) *StringSet {
+	// so we don't infinite loop
+	s.Set.UpdateFromList(values)
+
+	return s
+}
+
+// func StringSetSort(s *Set[string]) []string {
+
+// 	sortedGenes := make([]string, 0, len(s.items))
+
+// 	for key := range s.items {
+// 		sortedGenes = append(sortedGenes, key)
+// 	}
+
+// 	slices.Sort(sortedGenes)
+
+// 	return sortedGenes
+// }
