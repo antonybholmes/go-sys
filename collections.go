fbb615d098c9732c2f3792bd29f8e05d90d5d781
Antony Holmes <antony@antonyholmes.dev>
2025-11-18 16:20:06 -0500

    Fixed: Fixed super role not matching in rule engine

---
+++ collections.go
@@ -0,0 +1,22 @@
+package sys
+
+import "slices"
+
+func MapKeys[K comparable, V any](m map[K]V) []K {
+	keys := make([]K, 0, len(m))
+	for k := range m {
+		keys = append(keys, k)
+	}
+	return keys
+}
+
+func SortedMapKeys[K ~string, V any](m map[K]V) []K {
+	keys := make([]K, 0, len(m))
+	for k := range m {
+		keys = append(keys, k)
+	}
+
+	slices.Sort(keys)
+
+	return keys
+}
