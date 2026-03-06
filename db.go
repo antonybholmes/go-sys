13904325b1937981ea92e965480fc5988b7fba04
Antony Holmes <antony@antonyholmes.dev>
2026-03-06 17:26:37 -0500

    Added: Added support for audience in jwts

--- db.go
+++ db.go
@@ -15,7 +15,10 @@ type (
 const (
 	Sqlite3DB            = "sqlite3"
 	SqliteReadOnlySuffix = "?mode=ro"
-	PostgresDB           = "postgres"
-	MySQLDB              = "mysql"
-	BlankUUID            = "00000000-0000-0000-0000-000000000000"
+	// 32 MB cache size, 128 MB mmap size, and foreign keys on for good measure
+	SqliteDSN = "?mode=ro&immutable=1&_cache_size=-32768&_foreign_keys=on&_mmap_size=134217728"
+
+	PostgresDB = "postgres"
+	MySQLDB    = "mysql"
+	BlankUUID  = "00000000-0000-0000-0000-000000000000"
 )
