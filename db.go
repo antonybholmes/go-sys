65bf7de9a1334089d7015e653e6221978696b7e5
Antony Holmes <antony@antonyholmes.dev>
2026-03-11 14:06:27 -0400

    Changed: WGS tuned to reduce returned data and support multiple datasets per sample

--- db.go
+++ db.go
@@ -15,8 +15,10 @@ type (
 const (
 	Sqlite3DB            = "sqlite3"
 	SqliteReadOnlySuffix = "?mode=ro"
-	// 32 MB cache size, 128 MB mmap size, and foreign keys on for good measure
-	SqliteDSN = "?mode=ro&immutable=1&_cache_size=-32768&_foreign_keys=on&_mmap_size=134217728"
+	// 32 MB cache size, 128 MB mmap size, and foreign keys on for good measure, turn off foreign keys
+	// as they are not needed for read-only access and can cause performance issues with large datasets,
+	// and turn off synchronous for better performance as well as no disk syncs needed. Safe for read-only, slightly faster query execution.
+	SqliteDSN = "?mode=ro&immutable=1&_foreign_keys=OFF&_synchronous=OFF&_cache_size=-32768&_mmap_size=134217728"
 
 	PostgresDB = "postgres"
 	MySQLDB    = "mysql"
